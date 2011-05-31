5b72e4d4115b7bf3c5f7e2307a577711fff5f339
Eher <alexandre.eher@lbslocal.com>
2011-05-31 12:48:01 -0300

    adicionando as paradas do phpunit em um só lugar... ;)

---
+++ phpunit.php
@@ -0,0 +1,20 @@
+<?php
+
+$dirPath = __DIR__;
+
+$paths = array(
+  ini_get("include_path"),
+  $dirPath . '/dbunit',
+  $dirPath . '/php-file-iterator',
+  $dirPath . '/php-timer',
+  $dirPath . '/phpunit',
+  $dirPath . '/phpunit-selenium',
+  $dirPath . '/php-code-coverage',
+  $dirPath . '/php-text-template',
+  $dirPath . '/php-token-stream',
+  $dirPath . '/phpunit-mock-objects',
+);
+
+ini_set("include_path", implode(":", $paths)  );
+
+require "phpunit/phpunit.php";
